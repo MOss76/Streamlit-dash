5398349641a666031e95cc161d7fb826a280b929
MOss76 <103170854+MOss76@users.noreply.github.com>
2025-05-23 19:51:09 +0200

    app.py

--- app.py
+++ app.py
@@ -45,10 +45,7 @@ with st.sidebar:
     # Logo
     col1, col2, col3 = st.columns([1,2,1])
             with st.sidebar:
-    # Logo
-    col1, col2, col3 = st.columns([1,2,1])
-    with col2:
-        st.imagest.image("https://www.aiviewgroup.com/assets/img/logo/logo_ai-solutions.jpg", width=550)
+            st.imagest.image("https://www.aiviewgroup.com/assets/img/logo/logo_ai-solutions.jpg", width=550)
     
 
 
